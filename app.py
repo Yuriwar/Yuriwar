4ffb18466f5032351e98023464c87a7608c4ce83
Yuriwar <69070098@kmitl.ac.th>
2026-07-17 12:30:10 +0700

    banpisan func

---
+++ app.py
@@ -0,0 +1,14 @@
+"""Bannpisan2"""
+def main():
+    banpisan = input("name:")
+    if banpisan == "kla":
+        print("eskimo")
+    elif banpisan == "ramex":
+        print("PSCP")
+    elif banpisan == "toto":
+        print("BYD")
+    elif banpisan == "Yuriwar":
+        print("Pokemon master")
+    else:
+        print("not banpisan")
+main()
